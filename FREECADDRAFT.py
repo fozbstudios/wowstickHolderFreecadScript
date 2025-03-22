c77fdcf71bb7e85e8b2d6ed895157053e95c2930
Nick K <fozbstudios@gmail.com>
2025-03-22 16:24:08 -0500

    top poly holes in

--- FREECADDRAFT.py
+++ FREECADDRAFT.py
@@ -10,6 +10,7 @@ from BOPTools import BOPFeatures
 
 
 
+
 bitList=\
     ["PH0000","PH000","PH00","PH0","PH1","PH2","SL1.0","SL1.5","SL2.0","SL2.5","SL3.0","SL3.5","SL4.0",\
     "T2","T3","T4","T5","T6","T7","T8","T9","T10","T15","T20","P2","P5","P6","H0.7","H0.9","H1. 3","H1.5",\
@@ -17,14 +18,35 @@ bitList=\
     "U3.0","tri2.0","tri2.3","tri2.5","tri3.0","sim0.8","w1.5","extPH0","extPH2","extSL2.0","extH2.0"]
 curStr=iter(bitList)
 holeDia=8.3;holeRad=holeDia/2;holeWallThickness=1.5; holeDepth=8;bottomThickness=5; xPadding=3.5; yPadding=xPadding; xBitSpacing=4; yBitSpacing=xBitSpacing;textHoleSep=2
-xBitCount=7; yBitCount=8;boxXDist=(holeDia+xBitSpacing)*xBitCount+xPadding*2; boxYDist=(holeDia+yBitSpacing)*yBitCount+yPadding*2
+xBitCount=7; yBitCount=8;boxXDist=(holeDia+xBitSpacing)*xBitCount+xPadding*2; boxYDist=(holeDia+yBitSpacing)*yBitCount+yPadding*2;hexMaxRad=3
 boxZDist=holeDepth+bottomThickness; holeFilletRad=.6;boxFilletRolad=1.2; textHeight=6; fontSize=6; maxXdist=210;
 
 part=None
 solidTexts=[]
 holeWallCircles=[]
+topPolygons=[]
 holeNegativeCircles=[]
 shapestrings = []
+def extrude(obj,dist, reverse):
+    extrudeName=f'{obj.Label}Extrude'
+    doc.addObject('Part::Extrusion',extrudeName)
+    extrudeObj = doc.getObject(extrudeName)
+    extrudeObj.Base = obj
+    extrudeObj.DirMode = "Normal"
+    extrudeObj.DirLink = None
+    extrudeObj.LengthFwd= dist
+    extrudeObj.LengthRev = 0.000000000000000
+    extrudeObj.Solid = True
+    extrudeObj.Reversed = reverse
+    extrudeObj.Symmetric = False
+    extrudeObj.TaperAngle = 0.000000000000000
+    extrudeObj.TaperAngleRev = 0.000000000000000
+    # doc.getObject('Extrude').ViewObject.ShapeAppearance=getattr(doc.getObject('Fusion').getLinkedObject(True).ViewObject,'ShapeAppearance',doc.getObject('Extrude').ViewObject.ShapeAppearance)
+    # doc.getObject('Extrude').ViewObject.LineColor=getattr(doc.getObject('Fusion').getLinkedObject(True).ViewObject,'LineColor',doc.getObject('Extrude').ViewObject.LineColor)
+    # doc.getObject('Extrude').ViewObject.PointColor=getattr(doc.getObject('Fusion').getLinkedObject(True).ViewObject,'PointColor',doc.getObject('Extrude').ViewObject.PointColor)
+    obj.Visibility = False
+    # obj.Label=extrudeName
+    return extrudeObj
 def setParent(parent,child):
     parent.addObject(child)
     Gui.activeView().setActiveObject(part.Name, App.activeDocument().Part)
@@ -59,6 +81,7 @@ def addText(text,placement):
     shapestrings.append(ss)
 first=last=rowStart=prevText=None
 rightMost=bottomMost=0.0
+firsRow=True
 
 doc = FreeCAD.newDocument()
 bp = BOPFeatures.BOPFeatures(doc)
@@ -79,8 +102,10 @@ App.DraftWorkingPlane.setTop()
 for i,bitName  in enumerate(bitList):
     plm = FreeCAD.Placement()
     plm.Rotation.Q = (0.0, 0.0, 0.0, 1.0)
+    checkMakePoly=True
 
     if first is None:
+        checkMakePoly = False
         plm.Base = FreeCAD.Vector(0, 0, 0.0)
         addText(bitName,plm)
         rowStart=shapestrings[-1]
@@ -91,6 +116,9 @@ for i,bitName  in enumerate(bitList):
         addText(bitName,plm)
 
         if shapestrings[-1].Shape.BoundBox.XMax-rowStart.Shape.BoundBox.XMin>=maxXdist:
+            # go to next row
+            firsRow=False
+            checkMakePoly=False
             shapestrings[-1].Document.removeObject(shapestrings[-1].Name)
             del shapestrings[-1]
             prevText = rowStart
@@ -98,9 +126,10 @@ for i,bitName  in enumerate(bitList):
             plm.Base = FreeCAD.Vector(bb.XMin, bb.YMin - (holeDia + textHoleSep * 2 + yBitSpacing), 0.0)
             addText(bitName,plm)
             rowStart=shapestrings[-1]
-            bottomMost = rowStart.Shape.BoundBox.YMin-textHoleSep-holeDia
-            
+
+            bottomMost = rowStart.Shape.BoundBox.YMin-textHoleSep-holeDia-2*holeWallThickness
         bb=shapestrings[-1].Shape.BoundBox
+        rightMost=bb.XMax if bb.XMax>rightMost else rightMost
     plm = FreeCAD.Placement()
     plm.Rotation.Q = (0.0, 0.0, 0.0, 1.0)
     center= shapestrings[-1].Shape.BoundBox.Center
@@ -109,8 +138,27 @@ for i,bitName  in enumerate(bitList):
     circle=addCircle(holeRad+holeWallThickness,plm,label=f'{bitName}WallCircle')
     holeWallCircles.append(circle)
     circle=addCircle(holeRad,plm,label=f'{bitName}NegativeCircle')
-    holeNegativeCircles.append(circle)    
-      
+    holeNegativeCircles.append(circle)
+    if checkMakePoly:
+        FreeCAD.ActiveDocument.recompute()
+        leftMostPrev=holeWallCircles[-2].Shape.BoundBox.XMax
+        rightMostCur=holeWallCircles[-1].Shape.BoundBox.XMin
+        while rightMostCur-leftMostPrev>=4*hexMaxRad:
+            print(f"l: {leftMostPrev} r: {rightMostCur} c: { holeWallCircles[-2].Shape.BoundBox.Center.y} {holeWallCircles[-1].Label}")
+            hexMaxPlacement = FreeCAD.Placement()
+            hexMaxPlacement.Rotation.Q = (0.0, 0.0, 0.0, 1.0)
+            # hexMaxPlacement.Base = FreeCAD.Vector(5, 3, 0.0)
+            hexMaxPlacement.Base = FreeCAD.Vector(leftMostPrev+2*hexMaxRad, holeWallCircles[-2].Shape.BoundBox.Center.y, 0.0)
+            # hexMaxPlacement.Base = FreeCAD.Vector((rightMostCur-leftMostPrev)/2, holeWallCircles[-1].Shape.BoundBox.Center.y, 0.0)
+            hexMax = Draft.make_polygon(6, radius=hexMaxRad, inscribed=False, placement=hexMaxPlacement, face=False, support=None)
+            hexMax.Label=f'{bitName}HexMax'
+            setParent(parent=part,child=hexMax)
+            topPolygons.append(hexMax)
+            FreeCAD.ActiveDocument.recompute()
+            leftMostPrev=topPolygons[-1].Shape.BoundBox.XMax
+            print(f'{bitName}HexMax')
+
+    
 
             
 
@@ -119,13 +167,14 @@ for i,bitName  in enumerate(bitList):
 fbb=first.Shape.BoundBox
 leftMost=fbb.XMin-xPadding
 topMost=fbb.YMax+yPadding
-print(f'r{rightMost} l{leftMost} t{topMost} b{bottomMost}')
 rightMost+=xPadding
 bottomMost-=yPadding
 rectangle=makeBoundRect(leftMost,topMost,rightMost-leftMost,bottomMost-topMost)
-namesForFuse=[x.Name for x in shapestrings]
-namesForFuse.append(rectangle.Name)
-textAndRectFuse = bp.make_multi_fuse(namesForFuse)
+namesForTextRectFuse=[]
+namesForTextRectFuse=[x.Name for x in shapestrings]
+namesForTextRectFuse.extend([x.Name for x in topPolygons])
+namesForTextRectFuse.append(rectangle.Name)
+textAndRectFuse = bp.make_multi_fuse(namesForTextRectFuse)
 textAndRectFuse.Label='textAndRectFuse'
 setParent(parent=part,child=textAndRectFuse)
 holeWallFuse = bp.make_multi_fuse([x.Name for x in holeWallCircles])
@@ -133,15 +182,23 @@ holeWallFuse.Label='holeWallFuse'
 setParent(parent=part,child=holeWallFuse)
 holeNegativeFuse = bp.make_multi_fuse([x.Name for x in holeNegativeCircles])
 holeNegativeFuse.Label='holeNegativeFuse'
-
-setParent(parent=part,child=holeNegativeFuse)
+textAndRectExtrude=extrude(textAndRectFuse,3,True)
+holeWallExtrude=extrude(holeWallFuse,holeDepth+holeWallThickness*2,True)
+holeNegativeExtrude=extrude(holeNegativeFuse,holeDepth,True)
+setParent(parent=part,child=textAndRectExtrude)
+setParent(parent=part,child=holeWallExtrude)
+setParent(parent=part,child=holeNegativeExtrude)
+rectBitHoleFuse = bp.make_multi_fuse([textAndRectExtrude.Name,holeWallExtrude.Name])
+rectBitHoleFuse.Label='rectBitHoleFuse'
+topCut = bp.make_cut([rectBitHoleFuse.Name,holeNegativeExtrude.Name])
+topCut.Label='topCut'
 
 
 
 FreeCAD.ActiveDocument.recompute()
 
-exportName=f"D:/test{datetime.now().strftime('%d%b%Y-%H%M%S')}.step"
+# exportName=f"D:/test{datetime.now().strftime('%d%b%Y-%H%M%S')}.step"
 
-Import.export(shapestrings, exportName)
+# Import.export(shapestrings, exportName)
 
-del shapestrings
+# del shapestrings
